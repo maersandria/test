935dabca2c7478bb009ae635b35c49e894bb63d6
maers <maersdev@gmail.com>
2018-03-19 14:00:00 +0100

    tttt

---
+++ test.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+   
+   public void init (){
+        System.out.println("INITIATION singleton ");
+   }
+   public void destroy (){
+        System.out.println("destroy singleton ");
+   }
+
+   
+}
